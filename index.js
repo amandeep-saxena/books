89664604ffc0d02fd7c1e8a7cef0185256f08350
unknown <saxenaaman903@gmail.com>
2023-10-30 23:02:04 +0530

    redmi file

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.post('/books', async (req, res) => {
 
 
 
+//find all records//
+
 app.get('/books', async (req, res) => {
     const bookList = await BOOK.find();
     console.log(bookList);
@@ -59,23 +61,30 @@ app.get('/books', async (req, res) => {
 })
 
 
-
+//find records by id//
 
 app.get('/books/:id', async (req, res) => {
-
     const { id } = req.params;
-    await BOOK.findOne({ _id: id }).then((resp) => {
-        res.send(resp)
-    }).catch((error) => {
-        console.log(error)
-    })
+    const bookList = await BOOK.findOne({_id :id});
+    if(!bookList) return res.send('Book Do Not exist');
+    
+    console.log(bookList);
+    res.send(bookList);
+
+
+    // const { id } = req.params;
+    // await BOOK.findOne({ _id: id }).then((resp) => {
+    //     res.send(resp)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
     // res.send(book);
 
 });
 
 
 
-
+//delete records//
 
 app.delete('/books/:id', async function (req, res) {
     const { id } = req.params;
@@ -93,8 +102,29 @@ app.delete('/books/:id', async function (req, res) {
 });
 
 
+//update record//
 
 
+app.put('/books/:id', async function (req, res) {
+    const { id } = req.params;
+    const { title, author } = req.body;
+
+    const bookExist = await BOOK.findOne({_id : id});
+    if (!bookExist) return res.send('Book Do Not exist');
+
+    // // const updateField = (val, prev) => !val ? prev : val;
+
+    // const updatedBook = {
+    //     ...bookExist ,
+    //     title: updateField(title, bookExist.title),
+    //     author: updateField(author, bookExist.author),
+        
+    // };
+    await BOOK.updateOne({_id: id},{$set :{title , author}})
+    // await BOOK.updateOne({_id: id},{$set :{title : updatedBook.title, author: updatedBook.author}})
+    
+    res.status(200).send("Book Updated");
+});
 
 
 
